e2d1c6608cd8ed180cd051c9cbcd89bc9ad50e20
Yuri Ppo <yuri.prata12@gmail.com>
2024-12-28 16:37:42 -0800

    if else elif conditions implemented commit

--- main.py
+++ main.py
@@ -3,11 +3,24 @@ def main():
     print("Welcome to the Rollercoaster!")
     height = input("What's your height in cm? ")
     height_float = float(height)
+    age = input("What's your age? ")
+    age_int = int(age)
 
     if height_float >= 120:
-          print("Yes, you can ride the rollercoaster")
+            if age_int < 12:
+                  print("Yes, you can ride the rollercoaster")
+                  print("Please pay $5")
+            elif age_int >= 12 and age_int <= 18:
+                  print("Yes, you can ride the rollercoaster")
+                  print("Please pay $7")
+            else:
+                  print("Yes, you can ride the rollercoaster")
+                  print("Please pay $12")
+
+          
     else:
           print("No, you can't ride the rollercoaster")
+          print("Sorry you have to grow taller before you can ride")
     
 
 if __name__=="__main__":
